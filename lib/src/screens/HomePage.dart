579b556eb1738ead3f4b6eaa785eaf4d70f2e506
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-27 16:16:24 +0530

    Stack widget removed.

--- lib/src/screens/HomePage.dart
+++ lib/src/screens/HomePage.dart
@@ -22,14 +22,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new Stack(
-        children: <Widget>[
-        new Container(
+      body: Container(
           decoration: new BoxDecoration(
             image: new DecorationImage(image: new AssetImage("image_assets/background.png"), fit: BoxFit.cover,),),
+        child: Center(child:Text("tttt"),),
         ),
-        ],
-      ),
     );
   }
 }
