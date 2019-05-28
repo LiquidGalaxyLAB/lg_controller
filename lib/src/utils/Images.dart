8cc310a966b3bf5b048c23b9669cae21243f870a
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-28 23:37:45 +0530

    Added constants for image resources.

---
+++ lib/src/utils/Images.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class Images
+{
+  static const APP_LOGO=const AssetImage("image_assets/AppLogo.png");
+  static const BACKGROUND=const AssetImage("image_assets/background.png");
+  static const EARTH=const AssetImage("image_assets/Earth.png");
+  static const SUN=const AssetImage("image_assets/Sun.png");
+  static const MOON=const AssetImage("image_assets/Moon.png");
+  static const SKY=const AssetImage("image_assets/Sky.png");
+}
