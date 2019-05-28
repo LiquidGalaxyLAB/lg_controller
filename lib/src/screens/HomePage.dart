5cdf5c35c7a66ff4d76d2453047dc0205ce595da
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-28 23:41:00 +0530

    Added title bar to home page.

--- lib/src/screens/HomePage.dart
+++ lib/src/screens/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lg_controller/src/ui/ScreenBackground.dart';
+import 'package:lg_controller/src/ui/TitleBar.dart';
+import 'package:lg_controller/src/menu/MainMenu.dart';
 
 class HomePage extends StatefulWidget {
   HomePage();
@@ -14,7 +16,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Container(
           decoration: ScreenBackground.getBackgroundDecoration(),
-        child: Center(child:Text("home"),),
+        child: Center(child:new TitleBar(MainMenu.HOME),),
         ),
     );
   }
