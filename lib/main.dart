d33c7c5216dc4fff3ac49b89db3ea117fb562b13
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-29 00:11:33 +0530

    Update BLoC format.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lg_controller/src/states_events/PageActions.dart';
+import 'package:lg_controller/src/blocs/PageBloc.dart';
 import 'package:lg_controller/src/screens/HomePage.dart';
 import 'package:lg_controller/src/screens/GuidePage.dart';
 import 'package:lg_controller/src/screens/OverlayPage.dart';
@@ -20,8 +23,13 @@ var routes = <String, WidgetBuilder>{
   "/TourPage": (BuildContext context) => new TourPage()
 };
 
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  PageBloc pageBloc = PageBloc();
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +37,7 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft,
     ]);
-    return MaterialApp(
+    /*return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
           brightness: Brightness.light,
@@ -49,6 +57,43 @@ class MyApp extends StatelessWidget {
     ),
       ),
       home: HomePage(),
+    );*/
+    return BlocProvider<PageBloc>(
+      bloc: pageBloc,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.teal,
+          cardColor: Colors.white,
+          iconTheme: new IconThemeData(
+            color: Colors.white,
+            opacity: 1.0,
+          ),
+          fontFamily: 'RobotoMono',
+          textTheme: TextTheme(
+            headline: TextStyle(
+                fontSize: 34, color: Colors.white, fontWeight: FontWeight.bold),
+            title: TextStyle(
+                fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+            body1: TextStyle(fontSize: 12, color: Colors.white),
+            body2: TextStyle(
+                fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold),
+
+          ),
+        ),
+        home: BlocBuilder<PageEvent, PageState>(
+          bloc: pageBloc,
+          builder: (BuildContext context, PageState state) {
+            if (state is HomeState) {
+              return HomePage();
+            }
+            if (state is POIState) {
+              return POIPage();
+            }
+          },
+        ),
+      ),
     );
   }
 }
