ed5221095287905c21d8e3761ffb4743b48a0fc6
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-30 00:06:10 +0530

    Added page states

--- lib/src/states_events/PageActions.dart
+++ lib/src/states_events/PageActions.dart
@@ -1,7 +1,19 @@
-enum PageEvent { HOME, POI }
+enum PageEvent { HOME, POI, GUIDE, OVERLAY, PROFILE, SETTINGS, TOUR, ADDITIONAL }
 
 abstract class PageState {}
 
 class HomeState extends PageState {}
 
 class POIState extends PageState {}
+
+class GuideState extends PageState {}
+
+class OverState extends PageState {}
+
+class ProfileState extends PageState {}
+
+class SettingsState extends PageState {}
+
+class TourState extends PageState {}
+
+class TutorialState extends PageState {}
