1a54c4ad2b2e8d29b2729f264ef2a383fcf6e229
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-30 18:20:57 +0530

    Add home content view

--- lib/src/screens/HomePage.dart
+++ lib/src/screens/HomePage.dart
@@ -5,6 +5,8 @@ import 'package:lg_controller/src/menu/MainMenu.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:lg_controller/src/ui/HomeContent.dart';
+import 'package:lg_controller/src/models/POIData.dart';
 
 class HomePage extends StatefulWidget {
   HomePage();
@@ -21,7 +23,21 @@ class _HomePageState extends State<HomePage> {
     child:Scaffold(
       body: Container(
           decoration: ScreenBackground.getBackgroundDecoration(),
-        child: Center(child:new TitleBar(MainMenu.HOME),),
+        child: Center(
+          child:Column(
+            children: <Widget>[
+              SizedBox(
+                height: 64,
+          child:TitleBar(MainMenu.HOME),),
+              Expanded(
+                  child:Container(
+                    padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                  child:HomeContent(new POIData("Test","Trial description"))
+                  ),
+              ),
+            ]
+        ),
+        ),
         ),
     ),);
   }
