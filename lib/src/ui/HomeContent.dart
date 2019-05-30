fd286f1ef84cc241e41da92a4345dfd7adeace6f
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-30 18:21:10 +0530

    Create home content view

---
+++ lib/src/ui/HomeContent.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:lg_controller/src/ui/SearchBar.dart';
+import 'package:lg_controller/src/ui/NavigationView.dart';
+import 'package:lg_controller/src/ui/KMLDataView.dart';
+import 'package:lg_controller/src/models/KMLData.dart';
+
+class HomeContent extends StatelessWidget {
+
+  KMLData data;
+  HomeContent(this.data);
+
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SearchBar(),
+              KMLDataView(data),
+              Expanded(child: NavigationView(),),
+            ]
+        )
+
+    );
+  }
+}
