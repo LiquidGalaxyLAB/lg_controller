7c09bdbfaf733ad1b2c8452885d78eeb4bb1f519
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-30 18:21:48 +0530

    Create Navigation view(Touch pad)

---
+++ lib/src/ui/NavigationView.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:lg_controller/src/ui/SearchBar.dart';
+import 'package:lg_controller/src/models/KMLData.dart';
+
+class NavigationView extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return Container(
+        padding: new EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+        child:SizedBox(width:240,
+        child:Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(child:Card(child:Text("navigation view",style:Theme
+                  .of(context)
+                  .textTheme
+                  .title,),color:Colors.white),),
+              SizedBox(height: 8,),
+    ]
+    ),
+    ),);
+  }
+}
