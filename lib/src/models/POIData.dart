54d3d6cfe7c53d9ba444fc944e7a8c7b654677ab
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-30 18:20:33 +0530

    Create POI Data class

---
+++ lib/src/models/POIData.dart
@@ -0,0 +1,15 @@
+import 'package:lg_controller/src/models/KMLData.dart';
+
+class POIData extends KMLData
+{
+  POIData(String title, String desc) : super(title, desc);
+
+  String getTitle()
+  {
+    return super.getTitle();
+  }
+  String getDesc()
+  {
+    return super.getDesc();
+  }
+}
