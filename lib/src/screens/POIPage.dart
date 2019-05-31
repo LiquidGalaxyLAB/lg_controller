11435a4f7ba3e84bcc85fe818195e4678ba0a50a
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-05-31 17:53:33 +0530

    Update POI page

--- lib/src/screens/POIPage.dart
+++ lib/src/screens/POIPage.dart
@@ -3,6 +3,10 @@ import 'package:lg_controller/src/ui/ScreenBackground.dart';
 import 'package:lg_controller/src/ui/TitleBar.dart';
 import 'package:lg_controller/src/menu/MainMenu.dart';
 import 'package:flutter/services.dart';
+import 'package:lg_controller/src/ui/POIContent.dart';
+import 'package:lg_controller/src/models/POIData.dart';
+import 'package:lg_controller/src/ui/NavBar.dart';
+import 'package:lg_controller/src/ui/SearchBar.dart';
 
 class POIPage extends StatefulWidget {
   POIPage();
@@ -19,7 +23,38 @@ class _POIPageState extends State<POIPage> {
         child:Scaffold(
       body: Container(
         decoration: ScreenBackground.getBackgroundDecoration(),
-        child: Center(child:new TitleBar(MainMenu.POI),),
+        child: Center(
+          child:Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 64,
+                  child:TitleBar(MainMenu.POI),),
+                Expanded(
+                  child:Container(
+                      padding: EdgeInsets.fromLTRB(0,8,8,0),
+                      child:Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            SizedBox(
+                              width: 160,
+                              child: Container(
+                                color: Colors.blueGrey[800],
+                              child:NavBar(),),),
+                            Expanded(
+                                child:Column(
+                                  children: <Widget>[
+                                    SearchBar(),
+                                    Expanded(
+                                    child:POIContent(),),
+                                  ]),
+                              ),
+                            ]
+                      ),
+                  ),),
+
+              ]
+          ),
+        ),
       ),
     ),);
   }
