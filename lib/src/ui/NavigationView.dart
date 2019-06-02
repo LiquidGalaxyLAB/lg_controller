81dcdbca838a2b6d7a10a9587ce479de705fa5c6
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-02 10:32:13 +0530

    Added navigation view animation

--- lib/src/ui/NavigationView.dart
+++ lib/src/ui/NavigationView.dart
@@ -4,24 +4,29 @@ class NavigationView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: new EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
-      child: SizedBox(
-        width: 240,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            height: 8,
+          ),
+          Hero(
+            tag: "Nav_view",
+            child: SizedBox(
+              width: 220,
+              height: 100,
               child: Card(
                   child: Text(
                     "navigation view",
                     style: Theme.of(context).textTheme.title,
                   ),
-                  color: Colors.white),
+                  color: Colors.white70),
             ),
-            SizedBox(
-              height: 8,
-            ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+        ],
       ),
     );
   }
