13952d6fbc88f699f0d3c800ecc92df679b6d15d
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-03 23:27:17 +0530

    Created KMLFilesActions

---
+++ lib/src/states_events/KMLFilesActions.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:lg_controller/src/models/KMLData.dart';
+
+abstract class KMLFilesEvent extends Equatable {}
+
+class GET_FILES extends KMLFilesEvent {}
+
+abstract class KMLFilesState {}
+
+class UninitializedState extends KMLFilesState {}
+
+class ErrorState extends KMLFilesState {}
+
+class LoadingState extends KMLFilesState {}
+
+class LoadedState extends KMLFilesState {
+
+  List<KMLData> data;
+
+  LoadedState(this.data);
+}
