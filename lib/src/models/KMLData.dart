d6c20987f99cb10ca4791f4bb37ee7cee4354d62
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-10 09:03:19 +0530

    Added parameters and getter functions

--- lib/src/models/KMLData.dart
+++ lib/src/models/KMLData.dart
@@ -1,8 +1,17 @@
 class KMLData {
-  String title;
-  String desc;
+  String title = "";
+  String desc = "";
+  double latitude = 0, longitude = 0, bearing = 0, zoom = 0, tilt = 0;
+  int count = 0;
 
-  KMLData(this.title, this.desc);
+  KMLData(
+      {this.title,
+      this.desc,
+      this.latitude,
+      this.longitude,
+      this.bearing,
+      this.zoom,
+      this.tilt});
 
   getTitle() {
     return title;
@@ -12,18 +21,53 @@ class KMLData {
     return desc;
   }
 
+  getLat() {
+    return latitude;
+  }
+
+  getLgt() {
+    return longitude;
+  }
+
+  getBearing() {
+    return bearing;
+  }
+
+  getZoom() {
+    return zoom;
+  }
+
+  getTilt() {
+    return tilt;
+  }
+
   KMLData.fromJson(Map<String, dynamic> json)
       : title = json['title'],
-        desc = json['desc'];
+        desc = json['desc'],
+        latitude = json['latitude'],
+        longitude = json['longitude'],
+        bearing = json['bearing'],
+        zoom = json['zoom'],
+        tilt = json['tilt'];
 
   Map<String, dynamic> toJson() => {
         'title': title,
         'desc': desc,
+        'latitude': latitude,
+        'longitude': longitude,
+        'bearing': bearing,
+        'zoom': zoom,
+        'tilt': tilt,
       };
 
   Map<String, dynamic> toDatabaseMap() => {
         'title': title,
         'desc': desc,
+        'latitude': latitude,
+        'longitude': longitude,
+        'bearing': bearing,
+        'zoom': zoom,
+        'tilt': tilt,
         'count': 0,
       };
 }
