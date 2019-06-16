e8d4d041a1f1db1206137cd7c04e21365a0d276b
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-16 22:40:37 +0530

    Create KMLModuleView_test.dart

---
+++ test/widget_tests/KMLModuleView_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lg_controller/src/models/KMLData.dart';
+import 'package:lg_controller/src/states_events/NavBarActions.dart';
+import 'package:lg_controller/src/ui/KMLModuleView.dart';
+
+void main() {
+  testWidgets('KML module view component check', (WidgetTester tester) async {
+    Widget root = new KMLModuleView(
+        RecentlyState(), KMLData(title: "test", desc: "test2"));
+    await tester.pumpWidget(new Material(child: new MaterialApp(home: root)));
+
+    await tester.pumpAndSettle();
+
+    expect(find.text("test"), findsOneWidget);
+    expect(find.text("test2"), findsOneWidget);
+    expect(find.byType(CircleAvatar), findsOneWidget);
+    expect(find.byType(Card), findsOneWidget);
+    expect(
+        find.descendant(
+            of: find.byWidget(root), matching: find.byType(GestureDetector)),
+        findsOneWidget);
+  });
+}
