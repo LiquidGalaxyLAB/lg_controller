852800607233c2106332100f403a217c936a334b
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-02 23:54:16 +0530

    Add support for tours files

--- lib/src/gdrive/FileRequests.dart
+++ lib/src/gdrive/FileRequests.dart
@@ -3,33 +3,37 @@ import 'dart:convert';
 
 import 'package:googleapis/drive/v2.dart' as drive;
 import 'package:googleapis_auth/auth_io.dart' as auth;
-import 'package:lg_controller/src/menu/NavBarMenu.dart';
+import 'package:lg_controller/src/menu/MainMenu.dart';
+import 'package:lg_controller/src/menu/POINavBarMenu.dart';
+import 'package:lg_controller/src/menu/TourNavBarMenu.dart';
 import 'package:lg_controller/src/models/KMLData.dart';
+import 'package:lg_controller/src/models/POIData.dart';
 import 'package:lg_controller/src/models/SegregatedKMLData.dart';
+import 'package:lg_controller/src/models/TourData.dart';
 
 /// To handle all functionalities with Google Drive.
 class FileRequests {
   /// Credentials of the service account.
   final _credentials = r'''
   {
-  "private_key_id": "<>",
-  "private_key": "<>",
-  "client_email": "<>",
-  "client_id": "<>",
-  "type": "<>"
   }
   ''';
 
   /// Drive scope required for getting file data.
   final scopes = [drive.DriveApi.DriveScope];
 
-  /// To get the KML data for POI modules.
-  Future<Map<String, List<KMLData>>> getPOIFiles() async {
+  /// To get the KML data for the modules.
+  Future<Map<String, List<KMLData>>> getFiles(MainMenu pagestate) async {
     var client = await authorizeUser();
     if (client == null) return null;
     var api = new drive.DriveApi(client);
-    var query =
-        "mimeType = 'application/vnd.google-earth.kml+xml' and '1Gs-KiheWHACyUtYtvGZma8xsBX6r1iTJ' in parents";
+    String query = "";
+    if (pagestate == MainMenu.POI)
+      query =
+          "mimeType = 'application/vnd.google-earth.kml+xml' and '1Gs-KiheWHACyUtYtvGZma8xsBX6r1iTJ' in parents";
+    else if (pagestate == MainMenu.TOURS)
+      query =
+          "mimeType = 'application/vnd.google-earth.kml+xml' and '1bGjX92kaxTZ5zJxX5vLiAAyOM9zHV2sf' in parents";
     List<drive.File> files =
         await searchFiles(api, 24, query).catchError((error) {
       print('An error occured: ' + (error.toString()));
@@ -37,25 +41,45 @@ class FileRequests {
     }).whenComplete(() {
       client.close();
     });
-    return decodeFiles(files);
+    return decodeFiles(files, pagestate);
   }
 
   /// To get the KML data in required map format from [drive.File].
-  Future<Map<String, List<KMLData>>> decodeFiles(files) async {
+  Future<Map<String, List<KMLData>>> decodeFiles(
+      List<drive.File> files, MainMenu pagestate) async {
     Map<String, List<KMLData>> segData = new Map<String, List<KMLData>>();
-    for (var ic in NavBarMenu.values()) {
-      segData.addAll({ic.title: new List<KMLData>()});
-    }
-    SegregatedKmlData d;
-    try {
-      for (var file in files) {
-        d = new SegregatedKmlData.fromJson(jsonDecode(file.description));
-        if (segData.containsKey(d.category)) {
-          segData[d.category].add(KMLData.fromJson(jsonDecode(d.data)));
-        }
+    if (pagestate == MainMenu.POI) {
+      for (var ic in POINavBarMenu.values()) {
+        segData.addAll({ic.title: new List<KMLData>()});
       }
-    } catch (e) {
-      print(e.toString());
+      SegregatedKmlData d;
+      try {
+        for (var file in files) {
+          d = new SegregatedKmlData.fromJson(jsonDecode(file.description));
+          if (segData.containsKey(d.category)) {
+            segData[d.category].add(POIData.fromJson(jsonDecode(d.data)));
+          }
+        }
+      } catch (e) {
+        print(e.toString());
+      }
+    } else if (pagestate == MainMenu.TOURS) {
+      for (var ic in TourNavBarMenu.values()) {
+        segData.addAll({ic.title: new List<KMLData>()});
+      }
+      SegregatedKmlData d;
+      try {
+        for (var file in files) {
+          d = new SegregatedKmlData.fromJson(jsonDecode(file.description));
+          if (segData.containsKey(d.category)) {
+            Map<String, dynamic> data = jsonDecode(d.data);
+            data.addAll({'fileID': file.id});
+            segData[d.category].add(TourData.fromJson(data));
+          }
+        }
+      } catch (e) {
+        print(e.toString());
+      }
     }
     return segData;
   }
