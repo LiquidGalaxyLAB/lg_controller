d82d1bb495545ee55a147ff67c9edf33bdfefbd7
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-02 23:52:33 +0530

    Update KMLFilesBloc.dart

--- lib/src/blocs/KMLFilesBloc.dart
+++ lib/src/blocs/KMLFilesBloc.dart
@@ -1,11 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:lg_controller/src/gdrive/FileRequests.dart';
+import 'package:lg_controller/src/menu/MainMenu.dart';
 import 'package:lg_controller/src/models/KMLData.dart';
 import 'package:lg_controller/src/resources/SQLDatabase.dart';
 import 'package:lg_controller/src/states_events/KMLFilesActions.dart';
 
 /// Bloc structure for handling KML data.
 class KMLFilesBloc extends Bloc<KMLFilesEvent, KMLFilesState> {
+  /// State of currently open page/screen.
+  final MainMenu pagestate;
+
   /// Initial state of [KMLFilesBloc].
   @override
   KMLFilesState get initialState => UninitializedState();
@@ -16,22 +20,23 @@ class KMLFilesBloc extends Bloc<KMLFilesEvent, KMLFilesState> {
   /// Instance of [SQLDatabase] to get KML data from local storage.
   final SQLDatabase database;
 
-  KMLFilesBloc(this.fileRequests, this.database);
+  KMLFilesBloc(this.fileRequests, this.database, this.pagestate);
 
   @override
   Stream<KMLFilesState> mapEventToState(KMLFilesEvent event) async* {
     if (event is GET_FILES) {
       yield LoadingState();
-      Map<String, List<KMLData>> data = await database.getData();
+      Map<String, List<KMLData>> data = await database.getData(pagestate);
       if (data != null) {
         yield LoadedState(data);
       }
-      Map<String, List<KMLData>> dataNetwork = await fileRequests.getPOIFiles();
+      Map<String, List<KMLData>> dataNetwork =
+          await fileRequests.getFiles(pagestate);
       if (data != null && data.containsKey("Recently_Viewed"))
         dataNetwork["Recently_Viewed"] = data["Recently_Viewed"];
       if (dataNetwork != null) {
         yield LoadedState(dataNetwork);
-        await database.saveData(dataNetwork);
+        await database.saveData(dataNetwork, pagestate);
       }
     }
   }
