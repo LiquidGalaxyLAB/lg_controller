4fa3bb0924e102bb5c58aba2fbcd973bed24c419
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-02 23:52:07 +0530

    Refractor as nav bar for POI

---
+++ lib/src/menu/POINavBarMenu.dart
@@ -0,0 +1,26 @@
+import 'package:lg_controller/src/ui/NavBar.dart';
+
+/// Menu items for POI [NavBar].
+class POINavBarMenu {
+  final String title;
+
+  const POINavBarMenu(this.title);
+
+  static values() {
+    return [
+      RECENTLY_VIEWED,
+      CATEGORY_1,
+      CATEGORY_2,
+      CATEGORY_3,
+      CATEGORY_4,
+      CATEGORY_5
+    ];
+  }
+
+  static const POINavBarMenu RECENTLY_VIEWED = const POINavBarMenu("Recently_Viewed");
+  static const POINavBarMenu CATEGORY_1 = const POINavBarMenu("Category_1");
+  static const POINavBarMenu CATEGORY_2 = const POINavBarMenu("Category_2");
+  static const POINavBarMenu CATEGORY_3 = const POINavBarMenu("Category_3");
+  static const POINavBarMenu CATEGORY_4 = const POINavBarMenu("Category_4");
+  static const POINavBarMenu CATEGORY_5 = const POINavBarMenu("Category_5");
+}
