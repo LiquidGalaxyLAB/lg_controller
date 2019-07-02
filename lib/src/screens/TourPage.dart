385e4055d3dfdad7de7a870075224ecded8fdf69
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-02 23:58:54 +0530

    Update TourPage.dart

--- lib/src/screens/TourPage.dart
+++ lib/src/screens/TourPage.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lg_controller/src/blocs/KMLFilesBloc.dart';
+import 'package:lg_controller/src/blocs/NavBarBloc.dart';
+import 'package:lg_controller/src/gdrive/FileRequests.dart';
 import 'package:lg_controller/src/menu/MainMenu.dart';
+import 'package:lg_controller/src/resources/SQLDatabase.dart';
+import 'package:lg_controller/src/states_events/NavBarActions.dart';
+import 'package:lg_controller/src/ui/KMLGridContent.dart';
+import 'package:lg_controller/src/ui/NavBar.dart';
 import 'package:lg_controller/src/ui/ScreenBackground.dart';
+import 'package:lg_controller/src/ui/SearchBar.dart';
 import 'package:lg_controller/src/ui/TitleBar.dart';
 import 'package:lg_controller/src/utils/SizeScaling.dart';
 
@@ -14,6 +23,10 @@ class TourPage extends StatefulWidget {
 }
 
 class _TourPageState extends State<TourPage> {
+  final NavBarBloc nvBloc = NavBarBloc();
+  final KMLFilesBloc fBloc =
+      KMLFilesBloc(FileRequests(), SQLDatabase(), MainMenu.TOURS);
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -30,11 +43,43 @@ class _TourPageState extends State<TourPage> {
                   child: TitleBar(MainMenu.TOURS),
                 ),
                 Expanded(
-                  child: Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-                    child: Center(
-                      child: Text("tour"),
+                  child: BlocProviderTree(
+                    blocProviders: [
+                      BlocProvider<NavBarBloc>(bloc: nvBloc),
+                      BlocProvider<KMLFilesBloc>(bloc: fBloc),
+                    ],
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(0, 8, 8, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            width: 164 * SizeScaling.getWidthScaling(),
+                            child: Container(
+                              color: Colors.blueGrey[800],
+                              child: NavBar(MainMenu.TOURS),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 4 +
+                                4 * 0.5 * (SizeScaling.getWidthScaling() - 1),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                SearchWidget(),
+                                Expanded(
+                                  child: KMLGridContent(),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: 4 +
+                                4 * 0.5 * (SizeScaling.getWidthScaling() - 1),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -44,5 +89,16 @@ class _TourPageState extends State<TourPage> {
         ),
       ),
     );
+  }
+}
+
+/// Search bar of the Tour screen.
+class SearchWidget extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return SearchBar(
+        (() => BlocProvider.of<NavBarBloc>(context).dispatch(RECENTLY())),
+        ((searchText) => BlocProvider.of<NavBarBloc>(context)
+            .dispatch(SEARCH(searchText, MainMenu.TOURS))),
+        () => {});
   }
 }
