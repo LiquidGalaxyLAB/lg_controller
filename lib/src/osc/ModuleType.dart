e5942f5c71704b0687dbba8221dc019e7e15a7d8
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-02 23:54:44 +0530

    Update ModuleType.dart

--- lib/src/osc/ModuleType.dart
+++ lib/src/osc/ModuleType.dart
@@ -9,10 +9,12 @@ class ModuleType {
   const ModuleType(this.encoding, this.path);
 
   static values() {
-    return [EXIT, GESTURE, FLYTO, POI];
+    return [EXIT, GESTURE, FLYTO, POI, TOUR];
   }
+
   static const ModuleType EXIT = const ModuleType(-1, "/exit");
   static const ModuleType GESTURE = const ModuleType(0, "/gesture");
   static const ModuleType FLYTO = const ModuleType(1, "/flyto");
   static const ModuleType POI = const ModuleType(2, "/poi");
+  static const ModuleType TOUR = const ModuleType(3, "/tour");
 }
