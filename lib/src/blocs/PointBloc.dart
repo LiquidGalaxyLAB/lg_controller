dbed046a4d4d183ec65c097eec7331b77b70b6df
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-15 21:58:36 +0530

    Create PointBloc.dart

---
+++ lib/src/blocs/PointBloc.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+import 'package:bloc/bloc.dart';
+import 'package:lg_controller/src/menu/OverlayMenu.dart';
+import 'package:lg_controller/src/models/OverlayItem.dart';
+import 'package:lg_controller/src/models/PlacemarkData.dart';
+import 'package:lg_controller/src/states_events/PointActions.dart';
+
+/// Bloc structure for handling point touch actions.
+class PointBloc extends Bloc<PointEvent, PointState> {
+  @override
+  PointState get initialState => UninitializedState([]);
+
+  PointBloc();
+
+  /// Instance of a list of [OverlayItem] to store the overlays.
+  final List<OverlayItem> data = [];
+
+  @override
+  Stream<PointState> mapEventToState(PointEvent event) async* {
+    if (event is TAP_EVENT) {
+      if (event.menu == OverlayMenu.ROUND_TEMP) {
+        Random rnd = Random();
+        String id = String.fromCharCodes(
+            List<int>.generate(10, (i) => (97 + rnd.nextInt(26))));
+
+        data.add(PlacemarkData(event.point, id));
+        yield CompletedState(PlacemarkData(event.point, id));
+      } else {
+        yield UninitializedState(data);
+      }
+    } else if (event is CLEAR_EVENT) {
+      yield UninitializedState(data);
+    }
+  }
+}
