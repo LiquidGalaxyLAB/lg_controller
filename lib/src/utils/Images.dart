7234eb4e2a57b59fca971067ea398ce40707a01c
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-15 21:57:49 +0530

    Update Images.dart

--- lib/src/utils/Images.dart
+++ lib/src/utils/Images.dart
@@ -8,4 +8,5 @@ class Images {
   static const MARS = const AssetImage("image_assets/Mars.png");
   static const MOON = const AssetImage("image_assets/Moon.png");
   static const SKY = const AssetImage("image_assets/Sky.png");
+  static const TAP_GESTURE = const AssetImage("image_assets/tapGesture.gif");
 }
