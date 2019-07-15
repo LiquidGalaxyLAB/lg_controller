2a306ad2880908aec3dc613048c8f2b83b7f058a
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-15 22:06:18 +0530

    Update OverlayPage.dart

--- lib/src/screens/OverlayPage.dart
+++ lib/src/screens/OverlayPage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lg_controller/src/blocs/FreezeBloc.dart';
+import 'package:lg_controller/src/blocs/PointBloc.dart';
 import 'package:lg_controller/src/menu/MainMenu.dart';
+import 'package:lg_controller/src/ui/OverlayMapView.dart';
+import 'package:lg_controller/src/ui/OverlayMenuBar.dart';
 import 'package:lg_controller/src/ui/ScreenBackground.dart';
 import 'package:lg_controller/src/ui/TitleBar.dart';
 import 'package:lg_controller/src/utils/SizeScaling.dart';
@@ -14,11 +19,22 @@ class OverlayPage extends StatefulWidget {
 }
 
 class _OverlayPageState extends State<OverlayPage> {
+  PointBloc pBloc;
+  FreezeBloc fBloc;
+
+  @override
+  void initState() {
+    pBloc = PointBloc();
+    fBloc = FreezeBloc(pBloc);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () => SystemNavigator.pop(),
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
           decoration: ScreenBackground.getBackgroundDecoration(),
           child: Center(
@@ -29,11 +45,40 @@ class _OverlayPageState extends State<OverlayPage> {
                   child: TitleBar(MainMenu.OVERLAY),
                 ),
                 Expanded(
-                  child: Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-                    child: Center(
-                      child: Text("overlay"),
+                  child: BlocProviderTree(
+                    blocProviders: [
+                      BlocProvider<FreezeBloc>(bloc: fBloc),
+                      BlocProvider<PointBloc>(bloc: pBloc),
+                    ],
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                      child: Center(
+                        child: SizedBox(
+                          width: 540 * SizeScaling.getWidthScaling(),
+                          height: 280 * SizeScaling.getHeightScaling(),
+                          child: Card(
+                              elevation: 4,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Stack(
+                                  children: <Widget>[
+                                    OverlayMapView(),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        OverlayMenuBar(),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8)),
+                              color: Colors.white70),
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -43,5 +88,12 @@ class _OverlayPageState extends State<OverlayPage> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    pBloc.dispose();
+    fBloc.dispose();
+    super.dispose();
   }
 }
