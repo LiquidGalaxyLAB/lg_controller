a843c4be482112d3859b4b8aec3ba0ff2c08b8de
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-22 15:56:45 +0530

    Update SegregatedKMLData.dart

--- lib/src/models/SegregatedKMLData.dart
+++ lib/src/models/SegregatedKMLData.dart
@@ -3,7 +3,16 @@ class SegregatedKmlData {
   String data;
   String category;
 
+  /// Create [SegregatedKmlData] instance from JSON map.
   SegregatedKmlData.fromJson(Map<String, dynamic> json)
       : data = json['data'],
         category = json['category'];
+
+  /// Convert [SegregatedKmlData] instance to JSON map.
+  Map<String, dynamic> toJson() => {
+        'data': data,
+        'category': category,
+      };
+
+  SegregatedKmlData();
 }
