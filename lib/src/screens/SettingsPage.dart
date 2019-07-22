6c27359ceaa3adeb709a47a03303b2db1124d6a7
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-22 15:57:59 +0530

    Update SettingsPage.dart

--- lib/src/screens/SettingsPage.dart
+++ lib/src/screens/SettingsPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lg_controller/src/menu/MainMenu.dart';
 import 'package:lg_controller/src/ui/ScreenBackground.dart';
+import 'package:lg_controller/src/ui/SettingsContent.dart';
 import 'package:lg_controller/src/ui/TitleBar.dart';
 import 'package:lg_controller/src/utils/SizeScaling.dart';
 
@@ -19,6 +20,7 @@ class _SettingsPageState extends State<SettingsPage> {
     return WillPopScope(
       onWillPop: () => SystemNavigator.pop(),
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
           decoration: ScreenBackground.getBackgroundDecoration(),
           child: Center(
@@ -30,10 +32,37 @@ class _SettingsPageState extends State<SettingsPage> {
                 ),
                 Expanded(
                   child: Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                    padding: EdgeInsets.all(
+                        8 + (8 * 0.7 * SizeScaling.getWidthScaling() - 1)),
                     child: Center(
-                      child: Text("settings"),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Settings',
+                              style: Theme.of(context).textTheme.body2),
+                          Padding(padding: EdgeInsets.all(8)),
+                          Container(
+                            width: 400 +
+                                (400 * (SizeScaling.getWidthScaling() - 1)),
+                            height: 232 +
+                                (240 * (SizeScaling.getHeightScaling() - 1)),
+                            child: Card(
+                                elevation: 4,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: SingleChildScrollView(
+                                    child: Container(
+                                      margin: EdgeInsets.all(4),
+                                      child: SettingsContent(),
+                                    ),
+                                  ),
+                                ),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                color: Colors.white70),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
