b5f6366ef7a789b913b6c5a1827e3e332f704ba4
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-22 15:57:54 +0530

    Update ProfilePage.dart

--- lib/src/screens/ProfilePage.dart
+++ lib/src/screens/ProfilePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lg_controller/src/menu/MainMenu.dart';
+import 'package:lg_controller/src/ui/ProfileContent.dart';
 import 'package:lg_controller/src/ui/ScreenBackground.dart';
 import 'package:lg_controller/src/ui/TitleBar.dart';
 import 'package:lg_controller/src/utils/SizeScaling.dart';
@@ -19,6 +20,7 @@ class _ProfilePageState extends State<ProfilePage> {
     return WillPopScope(
       onWillPop: () => SystemNavigator.pop(),
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
           decoration: ScreenBackground.getBackgroundDecoration(),
           child: Center(
@@ -30,10 +32,32 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
                 Expanded(
                   child: Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                    padding: EdgeInsets.all(
+                        8 + (8 * 0.7 * SizeScaling.getWidthScaling() - 1)),
                     child: Center(
-                      child: Text("profile"),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            width: 400 +
+                                (400 * (SizeScaling.getWidthScaling() - 1)),
+                            height: 268 +
+                                (272 * (SizeScaling.getHeightScaling() - 1)),
+                            child: Card(
+                                elevation: 4,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Container(
+                                    margin: EdgeInsets.all(4),
+                                    child: ProfileContent(),
+                                  ),
+                                ),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                color: Colors.white70),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
