39964290ef26189c98ffede8e47a17a63ef3204e
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-22 15:57:01 +0530

    Update PointBloc.dart

--- lib/src/blocs/PointBloc.dart
+++ lib/src/blocs/PointBloc.dart
@@ -4,6 +4,8 @@ import 'package:bloc/bloc.dart';
 import 'package:lg_controller/src/menu/OverlayMenu.dart';
 import 'package:lg_controller/src/models/OverlayItem.dart';
 import 'package:lg_controller/src/models/PlacemarkData.dart';
+import 'package:lg_controller/src/models/PointData.dart';
+import 'package:lg_controller/src/models/LineData.dart';
 import 'package:lg_controller/src/states_events/PointActions.dart';
 
 /// Bloc structure for handling point touch actions.
@@ -16,6 +18,9 @@ class PointBloc extends Bloc<PointEvent, PointState> {
   /// Instance of a list of [OverlayItem] to store the overlays.
   final List<OverlayItem> data = [];
 
+  /// To store have finished [OverlayItem].
+  OverlayItem temp;
+
   @override
   Stream<PointState> mapEventToState(PointEvent event) async* {
     if (event is TAP_EVENT) {
@@ -24,16 +29,34 @@ class PointBloc extends Bloc<PointEvent, PointState> {
         String id = String.fromCharCodes(
             List<int>.generate(10, (i) => (97 + rnd.nextInt(26))));
 
-        data.add(PlacemarkData(event.point, id, "Default", "Def", 0));
+        data.add(
+            PlacemarkData(PointData(event.point, 0), id, "Default", "Def"));
         yield CompletedState();
+      } else if (event.menu == OverlayMenu.LINE) {
+        if (temp == null) {
+          Random rnd = Random();
+          String id = String.fromCharCodes(
+              List<int>.generate(10, (i) => (97 + rnd.nextInt(26))));
+          temp = LineData(id, "Default", "Def");
+          (temp as LineData).setPoint(
+            PointData(event.point, 0),
+          );
+        } else {
+          (temp as LineData).setPoint(
+            PointData(event.point, 0),
+          );
+          data.add(temp);
+          yield CompletedState();
+        }
       } else {
         yield UninitializedState(data);
       }
     } else if (event is CLEAR_EVENT) {
+      temp = null;
       yield UninitializedState(data);
     } else if (event is MODIFY_EVENT) {
+      temp = null;
       yield CompletedState();
-      yield UninitializedState(data);
     }
   }
 }
