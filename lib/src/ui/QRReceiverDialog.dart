68e11f52a6a2e52acd2cc254ab17a9c3d1977333
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-08-09 00:17:15 +0530

    Create QRReceiverDialog.dart

---
+++ lib/src/ui/QRReceiverDialog.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:get_ip/get_ip.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:lg_controller/src/osc/OSCActions.dart';
+import 'package:lg_controller/src/utils/SizeScaling.dart';
+
+/// Dialog to request OSC params.
+class QRReceiverDialog extends StatefulWidget {
+
+  final Function onReceive;
+  QRReceiverDialog(this.onReceive);
+
+  @override
+  _QRReceiverDialogState createState() => _QRReceiverDialogState();
+}
+
+class _QRReceiverDialogState extends State<QRReceiverDialog> {
+
+  /// To show loading of the dialog.
+  bool loading = false;
+
+  String data="";
+
+  @override
+  void initState() {
+    Random rnd = Random();
+    data = String.fromCharCodes(
+        List<int>.generate(5, (i) => (97 + rnd.nextInt(26))));
+    setData();
+    OSCActions().receiveShared(widget.onReceive);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        title: Text('Receive module.',
+            style: Theme.of(context).textTheme.title),
+        content: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Center(
+            child:SizedBox(
+              height: 200+ 200 * 0.5 * (SizeScaling.getWidthScaling() - 1),
+            width: 200+ 200 * 0.5 * (SizeScaling.getWidthScaling() - 1),
+            child: QrImage(
+                data: data,
+                size: 200 + 200 * 0.8 * (SizeScaling.getWidthScaling() - 1),
+              ),
+            ),
+          ),
+      ),
+    );
+  }
+
+  setData() async{
+    data=await GetIp.ipAddress + ',' + data;
+    print(data);
+    setState(() {});
+  }
+}
