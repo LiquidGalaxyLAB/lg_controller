4122c8873d4d0fce510f34925587db75a1077c15
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-08-09 00:29:43 +0530

    Add OSC message decoding

--- lib/src/osc/OSCMessage.dart
+++ lib/src/osc/OSCMessage.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:lg_controller/src/osc/ModuleType.dart';
 
 /// Handles framing of OSC Message.
 class OSCMessage {
@@ -47,5 +48,38 @@ class OSCMessage {
     ByteData byteData = new ByteData.view(list.buffer);
     byteData.setInt32(0, id);
     return list;
+  }
+
+  /// Convert integer from byte list.
+  int getInt(List<int> value) {
+    final buffer = Uint8List.fromList(value).buffer;
+    final byteData = new ByteData.view(buffer);
+    return byteData.getInt32(0);
+  }
+
+  /// Decode the complete OSC message in required format.
+  static OSCMessage fromBytes(List<int> bytes) {
+    try {
+      String path = utf8.decode(bytes.sublist(0, bytes.indexOf(0)));
+      ModuleType mod = ModuleType.SHARE;
+      if (path.compareTo("/lgcontroller" + mod.path) != 0) return null;
+      int shift = bytes.indexOf(0);
+      bytes =
+          bytes.sublist(shift + ((shift % 4 == 0) ? 0 : 4 - (shift % 4)) + 1);
+      String key = utf8.decode(bytes.sublist(0, bytes.indexOf(0)));
+      if (key.compareTo("iis") != 0) return null;
+      shift = bytes.indexOf(0);
+      bytes = bytes.sublist(shift);
+      int id = ByteData.view(Uint8List.fromList(bytes.sublist(0, 8)).buffer)
+          .getInt32(0);
+      int enc = ByteData.view(Uint8List.fromList(bytes.sublist(8, 12)).buffer)
+          .getInt32(0);
+      if (enc != mod.encoding) return null;
+      bytes = bytes.sublist(12);
+      String data = utf8.decode(bytes);
+      return OSCMessage(path: path, id: 0, encoding: mod.encoding, data: data);
+    } catch (e) {
+      return null;
+    }
   }
 }
