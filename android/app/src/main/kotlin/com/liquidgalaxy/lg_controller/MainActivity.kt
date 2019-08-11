ed58dfecb784a5d3ea0cd322acffda9b0c8d0b07
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-08-11 13:13:23 +0530

    Add intent filters and handlers for in app searches

--- android/app/src/main/kotlin/com/liquidgalaxy/lg_controller/MainActivity.kt
+++ android/app/src/main/kotlin/com/liquidgalaxy/lg_controller/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.liquidgalaxy.lg_controller
 
 import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.app.SearchManager
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    val intent = getIntent()
+    val action = intent.getAction()
+
+    if (Intent.ACTION_SEARCH.equals(action)) {
+      val query = intent.getStringExtra(SearchManager.QUERY)
+      Log.e("Search received",query)
+    }
   }
 }
